c4b6adddc8f5ca20a57791076fbb4852000c65e8
Artur <72747079+ArturM998@users.noreply.github.com>
2022-12-13 17:13:25 +0100

    login page

--- todolist/urls.py
+++ todolist/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 
+from .views import home_view
+
 app_name = "todolist"
 urlpatterns = [
+    path('', home_view, name="home"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from .forms import LoginForm
 
 # Create your views here.
+def login_view(request, *args, **kwargs):
+    form = LoginForm()
+    print(request.user.is_authenticated)
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        user = authenticate(username=request.POST['username'],password=request.POST['password'])
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+        else:
+            return redirect('/user/login/')
+    context = {"form": form}
+    return render(request, "users/login.html", context)
