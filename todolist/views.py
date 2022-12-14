8be2ec2c8ef6ee185d77814a7287a59bf912efb3
Artur <72747079+ArturM998@users.noreply.github.com>
2022-12-14 13:31:09 +0100

    readable tasks list

--- todolist/views.py
+++ todolist/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 
+from .models import Task
+
 # Create your views here.
 def home_view(request, *args, **kwargs):
-    context = {}
+    queryset = Task.objects.all().filter(owner=request.user)
+    context = {"tasks": queryset}
     return render(request, "todolist/home.html", context)
