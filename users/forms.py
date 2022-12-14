030213ab0fb58d8cb51ba72c0b4f3b595c709ae1
Artur <72747079+ArturM998@users.noreply.github.com>
2022-12-14 13:05:37 +0100

    register page

--- users/forms.py
+++ users/forms.py
@@ -3,3 +3,8 @@ from django import forms
 class LoginForm(forms.Form):
     username = forms.CharField(label='Username', max_length=25)
     password = forms.CharField(label='Password', max_length=25, widget=forms.PasswordInput())
+
+class RegisterForm(forms.Form):
+    username = forms.CharField(label='Username', max_length=25)
+    email = forms.EmailField(label="Email", max_length=25)
+    password = forms.CharField(label='Password', max_length=25, widget=forms.PasswordInput())
--- users/views.py
+++ users/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
-from .forms import LoginForm
+from django.contrib.auth.models import User
+
+from .forms import LoginForm, RegisterForm
 
 # Create your views here.
 def login_view(request, *args, **kwargs):
     form = LoginForm()
-    print(request.user.is_authenticated)
     if request.method == "POST":
         form = LoginForm(request.POST)
         user = authenticate(username=request.POST['username'],password=request.POST['password'])
@@ -16,3 +17,14 @@ def login_view(request, *args, **kwargs):
             return redirect('/user/login/')
     context = {"form": form}
     return render(request, "users/login.html", context)
+
+def register_view(request, *args, **kwargs):
+    form = RegisterForm()
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = User.objects.create_user(request.POST['username'], request.POST['email'], request.POST['password'])
+            user.save()
+            return redirect('/')
+    context = {"form": form}
+    return render(request, "users/register.html", context)
