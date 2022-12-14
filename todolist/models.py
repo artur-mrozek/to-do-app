6354d5bb21773a43fdc494e81ac5f9727d0281ed
Artur <72747079+ArturM998@users.noreply.github.com>
2022-12-14 13:17:58 +0100

    task model

--- todolist/models.py
+++ todolist/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.conf import settings
 
-# Create your models here.
+class Task(models.Model):
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=20)
+    description = models.CharField(max_length=300)
+    done = models.BooleanField(default=False)
