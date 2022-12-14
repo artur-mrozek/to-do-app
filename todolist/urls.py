a851b0f805ac1f660a74f3cca8de7dd7e6c9fc5e
Artur <72747079+ArturM998@users.noreply.github.com>
2022-12-14 14:00:08 +0100

    new task page

--- todolist/urls.py
+++ todolist/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import home_view
+from .views import home_view, create_taskt_view
 
 app_name = "todolist"
 urlpatterns = [
     path('', home_view, name="home"),
+    path('create/', create_taskt_view, name="home"),
 ]
--- todolist/views.py
+++ todolist/views.py
@@ -1,9 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import Task
+
+from .forms import TaskForm
 
 # Create your views here.
 def home_view(request, *args, **kwargs):
     queryset = Task.objects.all().filter(owner=request.user)
     context = {"tasks": queryset}
     return render(request, "todolist/home.html", context)
+
+def create_taskt_view(request, *args, **kwargs):
+    form = TaskForm(request.POST or None)
+    if form.is_valid():
+            t = Task(owner=request.user, title=request.POST["title"], description=request.POST["description"], done=request.POST.get("done") == "on")
+            t.save()
+            return redirect("/")
+    context = {"form": form}
+    return render(request, "todolist/create_task.html", context)
