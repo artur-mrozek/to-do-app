700308aedb3cc458146839c29b735fc6407b3a50
Artur <72747079+ArturM998@users.noreply.github.com>
2022-12-24 13:50:13 +0100

    styling for homepage

--- todolist/models.py
+++ todolist/models.py
@@ -6,7 +6,7 @@ from django.urls import reverse
 class Task(models.Model):
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=20)
-    description = models.CharField(max_length=300)
+    description = models.CharField(max_length=100)
     done = models.BooleanField(default=False)
 
     def get_absolute_url(self):
